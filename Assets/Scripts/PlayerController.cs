d96cf5679a559d51126d674d8b74a08508ee9863
Lucie Názrová <nazrova.lucie@centrum.cz>
2022-05-13 14:59:42 +0200

    skok hráče

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,56 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed;
+    private float strength = 5;
+    private bool spaceWasPressed;
+    private bool isGrounded;
+    
 
     private void Start()
     {
+        // zpøístupìní objektu hráèe
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // skákání po stisknutí mezerníku
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spaceWasPressed = true;      
+        }
     }
 
     // uplatòuje se fyzika
     private void FixedUpdate()
     {
+        //pohyb kamery v závislosti na pohybu hráèe
         float moveH = Input.GetAxis("Horizontal");
         float moveV = Input.GetAxis("Vertical");
-
         rb.AddForce(new Vector3(moveH, 0.0f, moveV) * speed);
+
+        // ošetøení nìkoliknásobného skoku - skok povolen jen, když je hráè na zemi
+        if (!isGrounded)
+        {
+            return;
+        }
+
+        // skok po stisknutí mezerníku
+        if (spaceWasPressed)
+        {
+            rb.AddForce(Vector3.up * strength, ForceMode.VelocityChange);
+            spaceWasPressed = false;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
     }
 }
