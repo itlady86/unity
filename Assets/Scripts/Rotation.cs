b694bf51c0e0d3b40277333f68e735ad5ee2cf87
Lucie Názrová <nazrova.lucie@centrum.cz>
2022-05-13 19:18:06 +0200

    úprava mincí, skripty pro otáčení

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3 (0,45) * Time.fixedDeltaTime);
+        
+    }
+}
